e5775992c364208d5f6e69c982ddd66996dd9c25
Sarge <sercan.altundas@gmail.com>
2019-10-13 22:13:52 +0300

    movement by keyboard

--- game.lisp
+++ game.lisp
@@ -9,7 +9,12 @@
 
 ; Load assets
 (gamekit:register-resource-package :keyword "assets/")
-(gamekit:define-image :player "moppy.png")
+
+(gamekit:define-image :player-right "moppy-right.png")
+(gamekit:define-image :player-left  "moppy-left.png")
+(gamekit:define-image :player-front "moppy-front.png")
+
+(gamekit:define-image :clouds "clouds.png")
 (gamekit:define-image :background "background.png")
 
 ; Start the game loop
@@ -17,34 +22,45 @@
 
 ; Variables
 (defvar *player-position* (gamekit:vec2 400 180))
-(defvar *target-position* (gamekit:vec2 0 0))
-(defvar *mouse-pressed* nil)
+(defvar *move-dir* 0)
+(defvar *speed* 2)
+(defvar *clouds-one-pos-x* 0)
+(defvar *clouds-two-pos-x* 800)
 
 ; Methods
-(defun update-position (position)
-  (if (> (gamekit:x position) (gamekit:x *target-position*)) (decf (gamekit:x position) 2))
-  (if (< (gamekit:x position) (gamekit:x *target-position*)) (incf (gamekit:x position) 2)))
+(defun update-position ()
+  (setf (gamekit:x *player-position*) (+ (* *move-dir* *speed*) (gamekit:x *player-position*))))
 
 ; Game logic
 (defmethod gamekit:draw ((app :the-game))
   (gamekit:draw-image (gamekit:vec2 0 0) :background)
-  (gamekit:draw-image *player-position* :player)
-  (gamekit:print-text (write-to-string (gamekit:x *target-position*)) 10 590)
-  (gamekit:print-text (write-to-string (gamekit:x *player-position*)) 10 570)
-  (gamekit:print-text (write-to-string *mouse-pressed*) 10 550))
+  (gamekit:draw-image (gamekit:vec2 *clouds-one-pos-x* 430) :clouds)
+  (gamekit:draw-image (gamekit:vec2 *clouds-two-pos-x* 430) :clouds)
+
+  (case *move-dir* 
+    (1  (gamekit:draw-image *player-position* :player-right))
+    (-1 (gamekit:draw-image *player-position* :player-left))
+    (0  (gamekit:draw-image *player-position* :player-front)))
+  
+  (gamekit:print-text (write-to-string *move-dir*) 10 590)
+  (gamekit:print-text (write-to-string (gamekit:x *player-position*)) 10 570))
 
 (defmethod gamekit:act ((app :the-game))
-  (update-position *player-position*))
+  (update-position)
+  (if (< *clouds-one-pos-x* -800) (setf *clouds-one-pos-x* 0))
+  (decf *clouds-one-pos-x* 1)
+  (if (< *clouds-two-pos-x* 0) (setf *clouds-two-pos-x* 800))
+  (decf *clouds-two-pos-x* 1))
 
 ; Input bindings
-(gamekit:bind-cursor (lambda (x y)
-  (when *mouse-pressed*
-    (setf 
-      (gamekit:x *target-position*) x
-      (gamekit:y *target-position*) y))))
+(gamekit:bind-button :a :pressed
+  (lambda () (setf *move-dir* -1)))
 
-(gamekit:bind-button :mouse-left :pressed
-  (lambda () (setf *mouse-pressed* t)))
+(gamekit:bind-button :a :released
+  (lambda () (setf *move-dir* 0)))
 
-(gamekit:bind-button :mouse-left :released
-  (lambda () (setf *mouse-pressed* nil)))
+(gamekit:bind-button :d :pressed
+  (lambda () (setf *move-dir* 1)))
+
+(gamekit:bind-button :d :released
+  (lambda () (setf *move-dir* 0)))
